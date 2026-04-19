a0420f7f6b29db68eb8ed6fc1b198f239c213a3a
pitercoding <piterg.bio@gmail.com>
2026-04-19 21:48:20 +0200

    feat(aula3): aplica tipagem em arrays (string[] e number[])

---
+++ topico-2/aula-03/script.ts
@@ -0,0 +1,8 @@
+let nomes: string[] = ['Racha Cuca', 'Quase Nada', 'Poucas Trancas'];
+
+let idade: number[] = [90, 56, 32, 11];
+
+let numeros: Array<number> = [90, 56, 32, 11]; //Outra forma
+
+numeros.push(77);
+
