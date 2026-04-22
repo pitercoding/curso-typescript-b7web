203b864e2bd1cc5d402d36fc8d63e7e5207b2445
pitercoding <piterg.bio@gmail.com>
2026-04-22 16:53:56 +0200

    feat(aula05): configura rootDir e outDir para organização da saída JS

---
+++ topico-3-configuracoes/public/assets/js/button.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const botao = document.querySelector('button');
+botao.addEventListener('click', () => {
+    console.log('Clicou!');
+});
+//# sourceMappingURL=button.js.map
